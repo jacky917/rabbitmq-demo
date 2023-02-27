c6c1f407b7cff1abc17264c4b6d1150dae8c508e
jacky917 <psnjacky917@gmail.com>
2023-02-27 16:38:38 +0900

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module rabbitmq_demo
 
 go 1.17
+
+require github.com/rabbitmq/amqp091-go v1.7.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"strconv"
+	"time"
+)
 
 func main() {
-	fmt.Println("test")
+
+	conn, err := amqp.Dial("amqp://admin:123456@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"test", // name
+		false,  // durable
+		false,  // delete when unused
+		false,  // exclusive
+		false,  // no-wait
+		nil,    // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	for i := 0; i < 99999; i++ {
+		body := "Hello World! " + strconv.Itoa(i)
+		err = ch.PublishWithContext(ctx,
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		time.Sleep(time.Second * 1)
+		failOnError(err, "Failed to publish a message")
+		log.Printf(" [x] Sent %s\n", body)
+	}
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
 }
